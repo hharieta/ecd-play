4331e4161f33d2c6876c2ef52d4c5604ad6f76b0
hharieta <L20530228@cancun.tecnm.mx>
2023-05-11 03:09:33 -0500

    reformated

--- js/engine.js
+++ js/engine.js
@@ -1,35 +1,36 @@
-//obteniendo todos los elementos requeridos
-const start_btn = document.querySelector(".start_btn button");
+const start_button = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
-const restart_btn = document.querySelector(".buttons .restart");
-const quit_btn = document.querySelector(".buttons .quit");
+const restart_button = document.querySelector(".buttons .restart");
+const comeback_button = document.querySelector(".buttons .quit");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
 const timeCount = document.querySelector(".timer .timer_min");
 
-//si hace click en el boton de inicio
-start_btn.onclick = () =>{
-    info_box.classList.add("activeInfo"); //mostrar el info box
+
+
+start_button.onclick = () =>{
+    info_box.classList.add("activeInfo"); 
 }
 
-quit_btn.onclick = ()  =>{
+comeback_button.onclick = ()  =>{
     window.location.href = "../pages/index.html";
 }
 
-//si hace click en el boton de continuar
-restart_btn.onclick = () =>{
-    info_box.classList.remove("activeInfo"); //ocultar el info box
-    quiz_box.classList.add("activeQuiz"); //mostrar el desafío
+
+
+restart_button.onclick = () =>{
+    info_box.classList.remove("activeInfo"); 
+    quiz_box.classList.add("activeQuiz"); 
     showQuestions(0);
-    QueCounter(1);
+    QuestionCounter(1);
     startTime(3600);
 }
 
-let selected_ques = 0;
-let que_cont = 0;
-let que_numb = 1;
+let selected_questions = 0;
+let questions_counter = 0;
+let question_number = 1;
 let counter;
-let userScore = 0;
+let user_score = 0;
 
 const next_btn = quiz_box.querySelector(".next_btn");
 const result_box = document.querySelector(".result_box");
@@ -68,29 +69,29 @@ shuffle(numbers);
 console.log(numbers);
 
 next_btn.onclick = () =>{
-    if(que_cont < questions_to_show - 1){
-        que_cont++;
-        que_numb++;
-        selected_ques = numbers[que_cont];
-        console.log(selected_ques);
-        showQuestions(selected_ques);
-        QueCounter(que_numb);
+    if(questions_counter < questions_to_show - 1){
+        questions_counter++;
+        question_number++;
+        selected_questions = numbers[questions_counter];
+        console.log(selected_questions);
+        showQuestions(selected_questions);
+        QuestionCounter(question_number);
         next_btn.style.display = "none";
     } else{
         console.log("Questions completed!");
-        showResultBox();
+        showTotalBox();
     }
 }
 
-//obteniendo preguntas y opciones del array
+
 function showQuestions(index){
-    const que_text = document.querySelector(".que_text");
-    let que_tag = '<span>'+questions[index].numb+'. '+ questions[index].question +'</span>';
+    const question_text = document.querySelector(".que_text");
+    let question_tag = '<span>'+questions[index].numb+'. '+ questions[index].question +'</span>';
     let option_tag = '<div class = "option">'+questions[index].options[0]+'<span></span></div>'
                     +'<div class = "option">'+questions[index].options[1]+'<span></span></div>'
                     +'<div class = "option">'+questions[index].options[2]+'<span></span></div>'
                     +'<div class = "option">'+questions[index].options[3]+'<span></span></div>';
-    que_text.innerHTML = que_tag;
+    question_text.innerHTML = question_tag;
     option_list.innerHTML = option_tag;
     const option = option_list.querySelectorAll(".option");
     for(let i = 0; i < option.length; i++){
@@ -104,12 +105,12 @@ let CrossIcon = '<div class="icon cross"><i class="fa-solid fa-xmark"></i></div>
 
 //seleccion de pregunta
 function optionSelected(answer){
-    let userAns = answer.textContent;
-    let correctAns = questions[selected_ques].answer;
+    let user_answer = answer.textContent;
+    let correct_answer = questions[selected_questions].answer;
     let AllOptions = option_list.children.length;
-    if(userAns == correctAns){
-        userScore+=1;
-        console.log(userScore);
+    if(user_answer == correct_answer){
+        user_score+=1;
+        console.log(user_score);
         answer.classList.add("correct");
         console.log("Correct Answer");
         answer.insertAdjacentHTML("beforeend", TickIcon);
@@ -121,7 +122,7 @@ function optionSelected(answer){
 
         //si la respuesta es incorrecta automaticamente selecciona la correcta
         for(let i = 0; i < AllOptions; i++){
-            if(option_list.children[i].textContent == correctAns){
+            if(option_list.children[i].textContent == correct_answer){
                 option_list.children[i].setAttribute("class", "option correct");
                 option_list.children[i].insertAdjacentHTML("beforeend", TickIcon);
             }
@@ -135,24 +136,26 @@ function optionSelected(answer){
     next_btn.style.display = "block";
 }
 
-function showResultBox(){
-    info_box.classList.remove("activeInfo"); 
-    quiz_box.classList.remove("activeQuiz"); 
-    result_box.classList.add("activeResult"); 
+function showTotalBox(){
+    info_box.classList.remove("activeInfo"); //ocultar el info box
+    quiz_box.classList.remove("activeQuiz"); //ocultar el desafío
+    result_box.classList.add("activeResult"); //mostrar la ventana de resultados
     const ScoreText = result_box.querySelector(".score_text");
-    
-    
-    if(userScore >= 0 && userScore <= 34){
-        let scoreTag = "<span><p>\n Lo sentimos, solo tuviste <b>"+userScore+"</b> de <b>"+questions_to_show+"</b> \nTu calificación <b>NO</b> es aprobatoria. Debes reintentar.</p></span>";
+
+
+
+    if(user_score >= 0 && user_score <= 34){
+        let scoreTag = "<span><p>\n Lo sentimos, solo tuviste <b>"+user_score+"</b> de <b>"+questions_to_show+"</b> \nTu calificación <b>NO</b> es aprobatoria. Debes reintentar.</p></span>";
         ScoreText.innerHTML = scoreTag;
     }
 
-    if(userScore >= 35 && userScore <= 50){
+    if(user_score >= 35 && user_score <= 50){
         
         
-        let scoreTag = "<span><p>\n Obtuviste <b>"+userScore+"</b> de <b>"+questions_to_show+"</b> \nTu calificación <b>SI</b> es aprobatoria. Puedes continuar con el siguiente nivel.</p></span>";
+        let scoreTag = "<span><p>\n Obtuviste <b>"+user_score+"</b> de <b>"+questions_to_show+"</b> \nTu calificación <b>SI</b> es aprobatoria. Puedes continuar con el siguiente nivel.</p></span>";
         ScoreText.innerHTML = scoreTag;
     }
+
 }
 
 function startTime(time){
@@ -160,7 +163,6 @@ function startTime(time){
     function timer(){
         timeCount.textContent = time;
         time--;
-
         if(time < 999){
             let addZeros = timeCount.textContent; timeCount.textContent = "0" + addZeros;
         }
@@ -172,17 +174,16 @@ function startTime(time){
         }
         if(time < 0){
             clearInterval(counter);
-            timeCount.textContent = "000";
-            info_box.classList.remove("activeInfo");  quiz_box.classList.remove("activeQuiz"); result_box.classList.add("activeResult"); 
-            showResultBox();
+            timeCount.textContent = "0000"; info_box.classList.remove("activeInfo");  quiz_box.classList.remove("activeQuiz"); result_box.classList.add("activeResult"); 
+            showTotalBox();
         }
     }
 }
 
 //cuenta de las preguntas
-function QueCounter(index){
+function QuestionCounter(index){
     const total_questions = questions.length - 30;
-    const bottom_ques_conter = quiz_box.querySelector(".total_que");
-    let totalQueCountTag = '<span><p>'+ index +'</p>de<p>'+ total_questions +'</p>Preguntas</span>';
-    bottom_ques_conter.innerHTML = totalQueCountTag;
+    const bottom_questions_counter = quiz_box.querySelector(".total_que");
+    let total_questions_counter_tag = '<span><p>'+ index +'</p>de<p>'+ total_questions +'</p>Preguntas</span>';
+    bottom_questions_counter.innerHTML = total_questions_counter_tag;
 }
